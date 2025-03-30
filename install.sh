ca92d145096053d590f3f8835d1b1076125b83f3
zivoxRoot <theophiledefeche@ik.me>
2025-03-30 16:00:15 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ LATEST_RELEASE="0.0.0"
 DOWNLOAD_URL="https://github.com/$GITHUB_REPO/raw/refs/heads/main/bin/linux/md"
 
 # Download and install the executable
-echo "Downloading $APP_NAME and copying it to $DOWNLOAD_URL..."
+echo "Downloading $APP_NAME and copying it to $INSTALL_DIR..."
 sudo curl -L -o "$INSTALL_DIR/$APP_NAME" "$DOWNLOAD_URL"
 
 # Set permissions
